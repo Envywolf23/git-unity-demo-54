a0509339bc4b78452feb71b1eb3d1cfb99561d6b
Juan Pedro <pedrojuan133@hotmail.com>
2017-04-01 23:31:01 -0500

    UI Vidas Pedro

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,8 +3,23 @@
 public class Health : MonoBehaviour 
 {
 	public float lifePoints = 100f;
-	
-	public void ApplyDamage (float damage)
+    private int vidas = 3;
+
+    //Asigna o retorna las vidas del personaje
+    public int Vidas
+    {
+        get
+        {
+            return Vidas;
+        }
+
+        set
+        {
+            Vidas = value;
+        }
+    }
+
+    public void ApplyDamage (float damage)
 	{
 		lifePoints -= damage;
 		//TODO: Die
---
+++ Assets/Vidas/Hud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Hud : MonoBehaviour {
+
+    public Health Scripts;
+    public GameObject Barra_Vida;
+    private Animator anim;
+    public const string ESTADO_VIDAS = "Vidas";
+    
+	// Use this for initialization
+	void Start () {
+        anim = Barra_Vida.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetInteger(ESTADO_VIDAS , Scripts.Vidas);
+	}
+}
